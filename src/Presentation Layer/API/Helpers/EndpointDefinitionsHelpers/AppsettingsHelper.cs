a67d118057c49dd2079f4443429f0dd2073ce2af
Andreas Gkizis <75754279+AndreasGkizis@users.noreply.github.com>
2023-07-25 15:25:58 +0300

    Almost there?

--- src/Presentation Layer/API/Helpers/EndpointDefinitionsHelpers/AppsettingsHelper.cs
+++ src/Presentation Layer/API/Helpers/EndpointDefinitionsHelpers/AppsettingsHelper.cs
@@ -1,4 +1,7 @@
 using Newtonsoft;
+using Newtonsoft.Json.Linq;
+using System.Runtime.CompilerServices;
+
 namespace API.Helpers;
 public static class SettingsHelpers
 {
@@ -8,7 +11,10 @@ public static class SettingsHelpers
         {
             var filePath = Path.Combine(AppContext.BaseDirectory, "appsettings.json");
             string json = File.ReadAllText(filePath);
-            dynamic jsonObj = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+            //dynamic jsonObj = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+
+            JObject? jsonObj = JObject.Parse(json);
+            JToken? jsonToken = (JToken)jsonObj;
 
 
             var connectionStringValue = FindValueRecursively(connectionStringPath, jsonObj);
@@ -19,7 +25,7 @@ public static class SettingsHelpers
                 string sectionPathKey = $"Serilog:WriteTo[{i}]:Args:configureLogger:WriteTo[0]:Args:connectionString";
                 // configuration[$"Serilog:WriteTo:7:Args:configureLogger:WriteTo:0:Args:connectionString"] = logConnectionString;
 
-                SetValueRecursively(sectionPathKey, jsonObj, connectionStringValue);
+                SetValueRecursively(sectionPathKey, jsonToken, connectionStringValue);
 
             }
 
@@ -33,40 +39,8 @@ public static class SettingsHelpers
         }
     }
 
-    private static void SetValueRecursively<T>(string sectionPathKey, dynamic jsonObj, T value)
-    {
-        try
-        {
 
-            // split the string at the first ':' character
-            var remainingSections = sectionPathKey.Split(":", 2);
-
-            var currentSection = remainingSections[0];
-            // if (currentSection.ToString() == "WriteTo"){
-            //     System.Console.WriteLine(  "gocha");
-
-
-
-            // }
-            if (remainingSections.Length > 1)
-            {
-                // continue with the procress, moving down the tree
-                var nextSection = remainingSections[1];
-                SetValueRecursively(nextSection, jsonObj[currentSection], value);
-            }
-            else
-            {
-                // we've got to the end of the tree, set the value
-                jsonObj[currentSection] = value;
-            }
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Error while deserializing JSON: {ex.Message}");
-
-        }
-    }
-    private static object FindValueRecursively(string sectionPathKey, dynamic jsonObj)
+    private static string FindValueRecursively(string sectionPathKey, dynamic jsonObj)
     {
         // split the string at the first ':' character
         var remainingSections = sectionPathKey.Split(":", 2);
@@ -80,17 +54,86 @@ public static class SettingsHelpers
         }
         else
         {
-            // var bool1 =jsonObj[currentSection].ToString(); 
-            // var whatisit = jsonObj[currentSection];
-            // if (jsonObj[currentSection].ToString())
-            // {
-
-            // we've got to the end of the tree, get the value
             return jsonObj[currentSection].ToString();
-            // }else
-            // {
-            //     return new KeyNotFoundException("The Connection string specified does not exist in appsettings.json, maybe you forgot to rebuild?");
-            // }
         }
+    }
+
+
+    public static void SetValueRecursively(string sectionPathKey, JToken jsonToken, string value)
+    {
+        try
+        {
+            var jsonObj = (JObject)jsonToken;
+            // Split the string at the first ':' character
+            var remainingSections = sectionPathKey.Split(":", 2);
+            var currentSection = remainingSections[0];
+
+            if (IsArrayNotation(currentSection, out var arrayIndex))
+            {
+                GetArrayName(currentSection, out var arrayName);
+
+                var array = jsonObj[arrayName];
+
+                var arrayPositionObject = array[arrayIndex];
+
+                var nextSection = remainingSections[1];
+
+                SetValueRecursively(nextSection, arrayPositionObject, value);
+
+            }
+            else
+            {
+                jsonToken = jsonObj[currentSection];
+            }
+
+            if (remainingSections.Length > 1)
+            {
+                var nextSection = remainingSections[1];
+                SetValueRecursively(nextSection, jsonToken, value);
+            }
+            else
+            {
+                // We've got to the end of the tree, set the value!!
+                if (jsonToken.Type == JTokenType.String)
+                {
+                    var propToChange = jsonToken.Parent as JProperty;
+                    propToChange.Value = value;
+                }
+            }
+
+            //else if (jsonToken.Type == JTokenType.Array)
+            //{
+            //    var jsonArray = (JArray)jsonToken;
+            //    foreach (var item in jsonArray)
+            //    {
+            //        SetValueRecursively(sectionPathKey, item, value);
+            //    }
+            //}
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error while deserializing JSON: {ex.Message} at Json : {jsonToken}");
+        }
+    }
+    private static bool IsArrayNotation(string section, out int index)
+    {
+        index = -1;
+        if (section.EndsWith("]") && section.Contains("["))
+        {
+            var startIndex = section.IndexOf('[') + 1;
+            var endIndex = section.IndexOf(']');
+            if (int.TryParse(section.Substring(startIndex, endIndex - startIndex), out index))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static void GetArrayName(string section, out string name)
+    {
+        var sections = section.Split("[", 2);
+        name = sections[0];
+
     }
 }
